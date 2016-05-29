4429da3c731c119a91af8e9462b3c33bb4378f04
Jeffrey Huang <jeffreyrhuang@gmail.com>
2016-05-28 23:05:22 -0600

    add flash messages, user can post reviews, require authentication for review controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,6 @@
 class ReviewsController < ApplicationController
+  before_action :authenticate_user!
+
   def new
     @review = Review.new
     @restaurant = Restaurant.find(params[:restaurant_id])
@@ -9,12 +11,14 @@ class ReviewsController < ApplicationController
   end
 
   def create
-    @review = Review.new(review_params)
+    @review = current_user.reviews.build(review_params)
+    @review.restaurant_id = params[:restaurant_id]
     if @review.save
       flash[:success] = 'Review successfully saved'
       # fix redirect
       redirect_to root_path
     else
+      # if form is not valid then app crashes
       render 'new'
     end
   end
