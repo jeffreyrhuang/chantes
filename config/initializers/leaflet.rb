314a36f868ef808aed68a2ee6656b3eb18af7f40
Jeffrey Huang <jeffreyrhuang@gmail.com>
2016-06-25 19:58:30 -0600

    add map to restaurant new view and markers on maps

--- config/initializers/leaflet.rb
+++ config/initializers/leaflet.rb
@@ -1,5 +1,5 @@
 Leaflet.tile_layer = "http://{s}.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.png"
 Leaflet.subdomains = ['otile1', 'otile2', 'otile3', 'otile4']
-# You can also use any other tile layer here if you don't want to use Cloudmade - see http://leafletjs.com/reference.html#tilelayer for more
-Leaflet.attribution = "Your attribution statement"
-Leaflet.max_zoom = 18
+# You can also use any other tile layer here - see http://leafletjs.com/reference.html#tilelayer for more
+Leaflet.attribution = "Mapquest OSM"
+Leaflet.max_zoom = 20
