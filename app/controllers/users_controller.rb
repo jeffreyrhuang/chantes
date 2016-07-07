fdda6ba6ff492b84c6abf6b413395fe0b3c5ce80
Jeffrey Huang <jeffreyrhuang@gmail.com>
2016-07-07 15:10:26 -0600

    add avatar column to User model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,4 +5,16 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def user_params
+    params.require(:user).permit(:avatar)
+  end 
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,10 @@
 class User < ActiveRecord::Base
   has_many :reviews
+  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 end
