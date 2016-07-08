61912d443517079d55087e6be04d700aa5ca5c95
jeffreyrhuang <jeffreyrhuang@gmail.com>
2016-07-07 21:33:11 -0600

    schema.db out of sync, add foreign keys

--- db/schema.rb
+++ db/schema.rb
@@ -64,4 +64,6 @@ ActiveRecord::Schema.define(version: 20160707232541) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "reviews", "restaurants"
+  add_foreign_key "reviews", "users"
 end
