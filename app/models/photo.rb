8dfb5c207b104196dcea2b2c8da95432aca4ffd1
Jeffrey Huang <jeffreyrhuang@gmail.com>
2016-07-08 11:02:31 -0600

    add validations for photos and submit button for photo upload; display first photo in restaurant view

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -4,4 +4,8 @@ class Photo < ActiveRecord::Base
 
   has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: ""
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+  validates_attachment :image, content_type: { content_type: ["image/jpeg", "image/gif", "image/png"] }
+  validates :image, attachment_presence: true
+  validates_with AttachmentPresenceValidator, attributes: :image
+  validates_with AttachmentSizeValidator, attributes: :image, less_than: 1.megabytes
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,18 @@
 class User < ActiveRecord::Base
   has_many :reviews
   has_many :photos
-  has_attached_file :avatar, styles: { medium: "300x300>", small: "200x200>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  has_attached_file :avatar, styles: { medium: "300x300>", small: "200x200>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
+  validates_attachment :avatar, content_type: { content_type: ["image/jpeg", "image/gif", "image/png"] }
+  validates :avatar, attachment_presence: true
+  validates_with AttachmentPresenceValidator, attributes: :avatar
+  validates_with AttachmentSizeValidator, attributes: :avatar, less_than: 1.megabytes
 
   validates :first_name, presence: true
   validates :last_name, presence: true
